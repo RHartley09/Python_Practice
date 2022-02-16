14068406ece2557da09c5dffcea3a7703a877ab5
Bob Hartley <roberthartley09@gmail.com>
2022-02-16 14:57:10 -0600

    Add files via upload

---
+++ Practicing_Functions.py
@@ -0,0 +1,14 @@
+# This function will figure out if an integer is odd or even. Give it a try! #
+def Is_Odd_Even(number):
+    if number %2==0:
+        print("Even!")
+    elif number %2==1:
+        print("Odd!")
+    else:
+         print("This isn't a number.")
+
+# Call the function, and in the parameter, type an integer. #
+Is_Odd_Even(2)
+
+Is_Odd_Even(3)
+
